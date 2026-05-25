281bc0797f8aa6a5bdafb4a00567f44e086f0a5b
HanaF02 <139299804+HanaF02@users.noreply.github.com>
2026-05-25 16:10:32 +0300

    Remove comment from registerApi function

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,4 +1,4 @@
 import api from "./axios.js";
-export const registerApi = (data) => api.post("/api/users/register", data); //Axios automatically combines your base URL with this route path to execute a POST request
+export const registerApi = (data) => api.post("/api/users/register", data); 
 export const loginApi = (data) => api.post("/api/users/login", data);
 export const logoutApi = () => api.post("/api/users/logout");
